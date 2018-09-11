6c04614aee0feaa50e08bb8845df1036a922ab26
Bijon Miles <bijonmiles01@gmail.com>
2018-09-11 09:21:30 +0000

    automatically backed up by learn

--- levitation_quiz.rb
+++ levitation_quiz.rb
@@ -4,7 +4,9 @@ def levitation_quiz
 	loop do
 	  puts "What's up, Doc?"
 	  answer = gets.chomp
-	  break if answer == "STOP"
+	  if answer == "STOP"
+	    break
+	  end
 	 end
 	 puts "Okay, okay, jeez. I'll stop. Sorry."
 end
